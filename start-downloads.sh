48d7a3bbd901c3d6a91399a5b49eef8ea9f0e293
sir106 <sir106@users.noreply.github.com>
2026-05-14 08:08:16 +0200

    minors

--- start-downloads.sh
+++ start-downloads.sh
@@ -7,20 +7,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd "$SCRIPT_DIR"
 
 # Run the python script, passing all arguments to it
-#echo [START] Starting download for "Technology Review..."
+#echo [start-downloads.sh] Starting download for "Technology Review..."
 #python3 heis-o-mat.py -v tr "$@"
 
-#echo [START] Starting download for "Make..."
-#python3 heise-download.py -v make "$@"
+#echo [start-downloads.sh] Starting download for "Make..."
+#python3 heis-o-mat.py -v make "$@"
 
-echo [START] Starting download for "c't..."
-python3 heise-download.py -v ct "$@"
+echo [start-downloads.sh] Starting download for "c't..."
+python3 heis-o-mat.py -v ct "$@"
 
-#echo [START] Starting download for "IX..."
-#python3 heise-download.py -v ix "$@"
+#echo [start-downloads.shART] Starting download for "IX..."
+#python3 heis-o-mat.py -v ix "$@"
 
-#echo [START] Starting download for "C't Foto..."
-#python3 heise-download.py -v ct-foto "$@"
+#echo [start-downloads.sh] Starting download for "C't Foto..."
+#python3 heis-o-mat.py -v ct-foto "$@"
 
-#echo [START] Starting download for "Mac & I..."
-#python3 heise-download.py -v mac-and-i "$@"
+#echo [start-downloads.sh] Starting download for "Mac & I..."
+#python3 heis-o-mat.py -v mac-and-i "$@"
