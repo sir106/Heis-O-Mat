678bdd97508e023d523db2a4b24ddbf690418100
sir106 <sir106@users.noreply.github.com>
2026-05-14 08:34:01 +0200

    cron updated

--- start-downloads.sh
+++ start-downloads.sh
@@ -7,20 +7,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd "$SCRIPT_DIR"
 
 # Run the python script, passing all arguments to it
-#echo [start-downloads.sh] Starting download for "Technology Review..."
-#python3 heis-o-mat.py -v tr "$@"
+echo [start-downloads.sh] Starting download for "Technology Review..."
+python3 heis-o-mat.py -v tr "$@"
 
-#echo [start-downloads.sh] Starting download for "Make..."
-#python3 heis-o-mat.py -v make "$@"
+echo [start-downloads.sh] Starting download for "Make..."
+python3 heis-o-mat.py -v make "$@"
 
 echo [start-downloads.sh] Starting download for "c't..."
 python3 heis-o-mat.py -v ct "$@"
 
-#echo [start-downloads.shART] Starting download for "IX..."
-#python3 heis-o-mat.py -v ix "$@"
+echo [start-downloads.shART] Starting download for "IX..."
+python3 heis-o-mat.py -v ix "$@"
 
-#echo [start-downloads.sh] Starting download for "C't Foto..."
-#python3 heis-o-mat.py -v ct-foto "$@"
+echo [start-downloads.sh] Starting download for "C't Foto..."
+python3 heis-o-mat.py -v ct-foto "$@"
 
-#echo [start-downloads.sh] Starting download for "Mac & I..."
-#python3 heis-o-mat.py -v mac-and-i "$@"
+echo [start-downloads.sh] Starting download for "Mac & I..."
+python3 heis-o-mat.py -v mac-and-i "$@"
